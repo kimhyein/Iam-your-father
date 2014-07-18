dd58f76fddddaf0540255c3885807889cee99b07
kimhyein <khi1246@naver.com>
2014-07-18 20:39:54 +0900

    DD

--- app/src/main/java/com/example/at/accountbook/SmsList.java
+++ app/src/main/java/com/example/at/accountbook/SmsList.java
@@ -6,41 +6,50 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.telephony.SmsMessage;
 import android.util.Log;
+import android.widget.Toast;
 
 /**
  * Created by AT on 2014-07-13.
  */
 public class SmsList extends BroadcastReceiver{
 
-    private static final String ROSA_ACTION_SMS_RECEIVED = "android.provider.Telephony.SMS_RECEIVED";
+    static final String logTag = "SmsReceiver";
+   static final String ACTION = "android.provider.Telephony.SMS_RECEIVED";
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        Log.w("rosa", "test");
 
-        String test = intent.getAction();
 
-        if(test.equals(ROSA_ACTION_SMS_RECEIVED)){
-            Log.w("rosa", "test");
-            Bundle bundle = intent.getExtras();
-            if(bundle == null){
-                Log.w("rosa", "Message bundle is null !!");
-                return;
-            }else{
-                Object[] pdusObj = (Object[])bundle.get("pdus");
-                if(pdusObj == null){
-                    Log.w("rosa", "Message pdu's are null !!");
-                    return;
-                }
+       if(intent.getAction().equals(ACTION))
+       {
+           Bundle bundle = intent.getExtras();
+           if(bundle==null) {
+               return;
+           }
 
-                SmsMessage[] messages = new SmsMessage[pdusObj.length];
-                Log.i("rosa", "retrieving : " + pdusObj.length + "messages");
 
-                for(int i = 0 ; i < messages.length ; i++){
-                    messages[i] = SmsMessage.createFromPdu((byte[])pdusObj[i]);
-                    Log.i("rosa", messages[i].getOriginatingAddress()
-                            + "::" + messages[i].getMessageBody());
-                }
+        Object[] pdusObj = (Object[])bundle.get("pdus");
+        if(pdusObj == null){
+                 return;
+         }
+
+            SmsMessage[] smsMessages = new SmsMessage[pdusObj.length];
+            for(int i = 0; i < pdusObj.length; i++)
+            {
+                smsMessages[i] = SmsMessage.createFromPdu((byte[]) pdusObj[i]);
+                Log.e(logTag, "NEW SMS " + i + "th");
+                Log.e(logTag, "DisplayOriginatingAddress : " + smsMessages[i].getDisplayOriginatingAddress());
+                Log.e(logTag, "DisplayMessageBody : " + smsMessages[i].getDisplayMessageBody());
+                Log.e(logTag, "EmailBody : "+ smsMessages[i].getEmailBody());
+                Log.e(logTag, "EmailFrom : " + smsMessages[i].getEmailFrom());
+                Log.e(logTag, "OriginationAddress : " + smsMessages[i].getOriginatingAddress());
+                Log.e(logTag, "MessageBody : " + smsMessages[i].getMessageBody());
+                Log.e(logTag, "ServiceCenterAddress : " + smsMessages[i].getServiceCenterAddress());
+                Log.e(logTag, "TimestampMillis : " + smsMessages[i].getTimestampMillis());
+
+                Toast.makeText(context, smsMessages[i].getMessageBody(),Toast.LENGTH_LONG).show();
+
+
             }
         }
     }
